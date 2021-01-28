7f7907ace25cbb67e031d3394b6efb7cc9dc8dc0
Valdas Žemaitis <valdas@idega.com>
2021-01-28 11:43:12 +0200

    Minor improvements

--- src/java/com/idega/portal/service/impl/UserServiceImpl.java
+++ src/java/com/idega/portal/service/impl/UserServiceImpl.java
@@ -203,11 +203,14 @@ public class UserServiceImpl extends DefaultSpringBean implements UserService {
 			//Get the IDO user
 			com.idega.user.data.User userIDO = null;
 			String userPersonalId = CoreConstants.EMPTY;
-			if (profile != null && !StringUtil.isEmpty(profile.getPersonalID())) {
+			if (profile != null && (!StringUtil.isEmpty(profile.getPersonalID()) || !StringUtil.isEmpty(profile.getPersonalId()))) {
 				userPersonalId = profile.getPersonalID();
+				userPersonalId = StringUtil.isEmpty(userPersonalId) ? profile.getPersonalId() : userPersonalId;
 				userIDO = getUserByPersonalId(userPersonalId);
-			} else {
+			}
+			if (userIDO == null) {
 				userIDO = getLegacyUser(user);
+				getLogger().info("Editing profile (" + profile + ") of " + user);
 			}
 
 			//Change the user data
@@ -217,13 +220,12 @@ public class UserServiceImpl extends DefaultSpringBean implements UserService {
 				if (dataToLoad.contains(DataElement.ALL) || dataToLoad.contains(DataElement.GENERAL)) {
 					if (!StringUtil.isEmpty(profile.getName())) {
 						userIDO.setFullName(profile.getName());
-						userIDO.store();
 					}
 
-					if (!StringUtil.isEmpty(profile.getPersonalID())) {
-						userIDO.setPersonalID(profile.getPersonalID());
-						userIDO.store();
+					if (!StringUtil.isEmpty(userPersonalId)) {
+						userIDO.setPersonalID(userPersonalId);
 					}
+					userIDO.store();
 				}
 
 				//**** Change member username and password *****
